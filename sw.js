fd7b3f9b764683c9efc3f89e8a26b59e2b4c314d
3Dsly <7442490+3Dsly@users.noreply.github.com>
2026-08-30 13:21:55 -0400

    Fix critical null reference crash on done tasks and bump cache to v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tasks-v4';
+const CACHE = 'tasks-v5';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', e => {
